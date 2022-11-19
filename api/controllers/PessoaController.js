c813008cd9b589c12209952b111c119a97607a97
richard rosenblat <rosenblatr@gmail.com>
2022-11-19 14:36:02 -0300

    CRUD com Sequelize

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,14 +1,55 @@
 import database from "../models/index.js";
 
 class PessoaController {
-    static async PegaTodasAsPessoas(_req, res) {
+    static async pegaTodasAsPessoas(_req, res) {
         try {
             const todasAsPessoas = await (await database).Pessoas.findAll();
             return res.status(200).json(todasAsPessoas);
         } catch (error) {
-            return res.status(500).json(error.message)
+            return res.status(500).json(error.message);
+        }
+    }
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            const umaPessoa = await (await database).Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(umaPessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body;
+        try {
+            const novaPesssoaCriada = await (await database).Pessoas.create(novaPessoa);
+            return res.status(201).json(novaPesssoaCriada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+        try {
+            await (await database).Pessoas.update(novasInfos, { where: { id: Number(id) } });
+            const pesssoaAtualizada = await (
+                await database
+            ).Pessoas.findOne({ where: { id: Number(id) } });
+
+            return res.status(201).json(pesssoaAtualizada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+    static async excluiPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            await (await database).Pessoas.destroy({ where: { id: Number(id) } });
+            return res.status(204).send();
+        } catch (error) {
+            return res.status(500).json(error.message);
         }
     }
 }
 
-export default PessoaController
+export default PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,6 +3,10 @@ import PessoaController from '../controllers/PessoaController.js'
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.PegaTodasAsPessoas)
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.excluiPessoa)
 
 export default router
